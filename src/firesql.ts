37fff11fe75ef256825bc8f22b283c3c29a4b50f
lior ezra <lior.ez@gmail.com>
2020-07-22 14:37:21 +0300

    Remove necessary package

--- src/firesql.ts
+++ src/firesql.ts
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-import 'firebase/firestore';
+// import 'firebase/firestore';
 import { FireSQLOptions, QueryOptions } from './shared';
 import { parse } from './sql-parser';
 import { DocumentData, assert } from './utils';
